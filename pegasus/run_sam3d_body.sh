84525c7d617de6993f6fe44aa45d87082f59ca6f
chenkaixu <chenkaixusan@gmail.com>
2026-01-29 21:58:29 +0900

    现在开22个node，一个里面跑一个人的四个环境

--- pegasus/run_sam3d_body.sh
+++ pegasus/run_sam3d_body.sh
@@ -3,7 +3,7 @@
 #PBS -q gen_S
 #PBS -l elapstim_req=24:00:00
 #PBS -N sam3d_4nodes_run
-#PBS -t 0-10                           # 11個のジョブ配列を定義（0から10までの11ジョブ）
+#PBS -t 0-21                           # 22个
 #PBS -o logs/pegasus/sam3d_group_${PBS_SUBREQNO}.log
 #PBS -e logs/pegasus/sam3d_group_${PBS_SUBREQNO}_err.log
 
@@ -22,19 +22,30 @@ conda activate /home/SKIING/chenkaixu/miniconda3/envs/sam_3d_body
 declare -A ID_MAP
 
 # 0から10までのキーにそれぞれリストを割り当て
-ID_MAP["0"]="[01,02]"
-ID_MAP["1"]="[03,04]"
-ID_MAP["2"]="[05,06]"
-ID_MAP["3"]="[07,08]"
-ID_MAP["4"]="[09,10]"
-ID_MAP["5"]="[11,12]"
-ID_MAP["6"]="[13,14]"
-ID_MAP["7"]="[15,16]"
-ID_MAP["8"]="[17,18]"
-ID_MAP["9"]="[19,20]"
-ID_MAP["10"]="[21,24]"
+ID_MAP["0"]="[01]"
+ID_MAP["1"]="[02]"
+ID_MAP["2"]="[03]"
+ID_MAP["3"]="[04]"
+ID_MAP["4"]="[05]"
+ID_MAP["5"]="[06]"
+ID_MAP["6"]="[07]"
+ID_MAP["7"]="[08]"
+ID_MAP["8"]="[09]"
+ID_MAP["9"]="[10]"
+ID_MAP["10"]="[11]"
+ID_MAP["11"]="[12]"
+ID_MAP["12"]="[13]"
+ID_MAP["13"]="[14]"
+ID_MAP["14"]="[15]"
+ID_MAP["15"]="[16]"
+ID_MAP["16"]="[17]"
+ID_MAP["17"]="[18]"
+ID_MAP["18"]="[19]"
+ID_MAP["19"]="[20]"
+ID_MAP["20"]="[21]"
+ID_MAP["21"]="[24]"
 
-# 現在のタスク用リストを取得 (PBS_SUBREQNO は 0-10 の値をとる想定)
+# 現在のタスク用リストを取得 (PBS_SUBREQNO は 0-21 の値をとる想定)
 PERSON_LIST=${ID_MAP[$PBS_SUBREQNO]}
 
 echo "Node Index: $PBS_SUBREQNO"
@@ -45,12 +56,13 @@ VIDEO_PATH="/work/SKIING/chenkaixu/data/drive/videos_split"
 RESULT_PATH="/work/SKIING/chenkaixu/data/drive/sam3d_body_results"
 CKPT_ROOT="/work/SKIING/chenkaixu/code/Drive_Face_Mesh_PyTorch/ckpt/sam-3d-body-dinov3"
 
-python -m SAM3Dbody.main \
+python -m SAM3Dbody.main_person_env \
     paths.video_path=${VIDEO_PATH} \
     paths.result_output_path=${RESULT_PATH} \
     model.root_path=${CKPT_ROOT} \
     infer.gpu="[0]" \
-    infer.workers_per_gpu=2 \
+    infer.workers_per_gpu=4 \
     infer.person_list="${PERSON_LIST}" \
 
 echo "🏁 Node ${PBS_SUBREQNO} finished at: $(date)"
+# 一个node里面跑一个人的4个环境，也就是4个worker
